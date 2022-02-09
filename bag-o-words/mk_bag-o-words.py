ea6148f42ebe37b53bc8da966c98a032aee36a41
BobBorges <robert.d.borges@gmail.com>
2022-02-09 14:29:16 +0100

    add comments

--- bag-o-words/mk_bag-o-words.py
+++ bag-o-words/mk_bag-o-words.py
@@ -2,22 +2,27 @@
 import os, json
 
 def main():
+    ### list all files in the corpus dir
     corpus_files = os.listdir('corpus')
+    ### initialize empty dictionary 
     bag = {}
 
-    for corpus_file in corpus_files:
-        with open(f'corpus/{corpus_file}', 'r') as txtfile:
-            txt_lines = txtfile.readlines()
-            for line in txt_lines:
-                line = line.strip()
-                words = line.split(' ')
-                for word in words:
-                    if word not in bag:
-                        bag[word] = 1
-                    else:
-                        bag[word] += 1
+    ### create bag of words
+    for corpus_file in corpus_files:                            # iterate over list of files
+        with open(f'corpus/{corpus_file}', 'r') as txtfile:     # open each file for reading
+            txt_lines = txtfile.readlines()                     # read each line to a list
+            for line in txt_lines:                              # iterate over list of lines
+                line = line.strip()                             # strip punctuation and returns
+                words = line.split(' ')                         # split line by space -> list of words
+                for word in words:                              # iterate over list of words
+                    if word not in bag:                         # if word not in bag dictionary
+                        bag[word] = 1                           # add it with value of 1
+                    else:                                       # or else
+                        bag[word] += 1                          # increment the existing val by 1
 
     print(bag)
+    #sort by val
+    #dump to a file
 
 
 if __name__ == '__main__':
